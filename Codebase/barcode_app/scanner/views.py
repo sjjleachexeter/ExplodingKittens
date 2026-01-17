789d30876e47dd37c4c2276f317f7ef1946c2817
olim88 <bobq4582@gmail.com>
2026-01-17 19:20:35 +0000

    commit missing files

---
+++ Codebase/barcode_app/scanner/views.py
@@ -0,0 +1,18 @@
+import json
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render
+
+
+
+# Create your views here.
+
+def index(request):
+
+    return render(request, "scanner/index.html")
+
+def scan_barcode(request):
+    data = json.loads(request.body)
+    barcode = data["barcode"]
+
+    # process barcode (lookup product, etc.)
+    return JsonResponse({"status": "ok", "barcode": barcode})
