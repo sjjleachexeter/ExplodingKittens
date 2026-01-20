9863422da977de34b570a87f43d56fd2d8653e67
sjjleachexeter <sl1169@exeter.ac.uk>
2026-01-20 11:37:46 +0000

    File structure changes

--- DjangoFramework/DjangoFramework/wsgi.py
+++ DjangoFramework/DjangoFramework/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for FoodforThought project.
+WSGI config for DjangoFramework project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FoodforThought.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoFramework.settings")
 
 application = get_wsgi_application()
