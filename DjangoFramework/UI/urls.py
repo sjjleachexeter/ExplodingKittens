13b2517915f73ae5b39e34b5b90c2942972e9c1d
SableTheRed <isobel@audioengineer.co.uk>
2026-01-25 16:35:07 +0000

    define UI routes

--- DjangoFramework/UI/urls.py
+++ DjangoFramework/UI/urls.py
@@ -2,5 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name='home')
+    # Basic navigation
+    path("", views.home, name='home'),
+    path("scan/", views.scan, name='scan'),
+    path("product/", views.product_passport, name='product'),
+    path("missions/", views.missions, name='missions'),
+    path("leaderboard/", views.leaderboard, name='leaderboard'),
+    path("profile/", views.profile, name='profile'),
+    path("user/", views.user, name='user'),
+    path("signup/", views.signup, name='signup'),
+    path("login/", views.login, name='login'),
+
+
+    # GDPR compliance
+    path("privacy/", views.privacy, name='privacy'),
+    path("terms-and-conditions/", views.terms, name='terms'),
+    path("about/", views.about, name='about')
 ]
--- DjangoFramework/UI/views.py
+++ DjangoFramework/UI/views.py
@@ -3,36 +3,72 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     '''
-    Homepage for application, probably where scanner is integrated (tho scanner could have own page too?)
+    Homepage for application
     '''
     return render(request, 'home.html')
+
+def scan(request):
+    '''
+    Scanner functionality is implemented on its own page
+    Avoids broken behaviour being displayed on home page if camera unavailable'''
+    return HttpResponse("Scan page")
 
 def product_passport(request):
     '''
     Will display product passport page
     '''
+    return HttpResponse("Product passport")
 
 def missions(request):
     '''
     Display user missions (shows progress etc)
     '''
+    return HttpResponse("Missions")
 
-def leaderboard(resuest):
+def leaderboard(request):
     '''
-    shows leaderboard with user position (based on user points)
+    Show leaderboard with user position (based on user points)
     '''
-
-def auth(request):
-    '''
-    possible authentication page
-    '''
-
-def about(request):
-    '''
-    gives app info? Might include links to T&Cs/privacy policy
-    '''
+    return HttpResponse("Leaderboard")
 
 def profile(request):
     '''
-    where user can adjust personal settings and stuff
+    Allow user to adjust personal & account settings
     '''
+    return HttpResponse("User's own profile")
+
+def user(request):
+    '''
+    Display information about a user
+    '''
+    return HttpResponse("Profile of another user")
+
+def signup(request):
+    '''
+    Allow user to create an account
+    '''
+    return HttpResponse("Signup page")
+
+def login(request):
+    '''
+    Allow user to log in
+    '''
+    return HttpResponse("Login page")
+
+def privacy(request):
+    '''
+    Display privacy policy
+    '''
+    return HttpResponse("Privacy Policy")
+
+def terms(request):
+    '''
+    Display terms and conditions
+    '''
+    return HttpResponse("Terms and Conditions")
+
+def about(request):
+    '''
+    Display information about app (version etc.)
+    '''
+    return HttpResponse("Site info.")
