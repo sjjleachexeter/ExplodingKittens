bae53a311f8719fefac1c83ba20ad71baef3586a
Zachary-F201 <zjmf201@exeter.ac.uk>
2026-01-30 10:26:46 +0000

    Finished base code for logging in - Still needs error checking

--- login_page/settings.py
+++ login_page/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -121,4 +122,5 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
