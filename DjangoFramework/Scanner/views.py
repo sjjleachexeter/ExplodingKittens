ed54143c4b3677f37ec3bf2ca72de18dd629ac69
olim88 <bobq4582@gmail.com>
2026-02-10 15:42:14 +0000

    remove unneeded code

--- DjangoFramework/Scanner/views.py
+++ DjangoFramework/Scanner/views.py
@@ -8,10 +8,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "Scanner/scan.html")
-
-def scan_barcode(request):
-    data = json.loads(request.body)
-    barcode = data["barcode"]
-    print(barcode)
-    # process barcode (lookup product, etc.)
-    return JsonResponse({"status": "ok", "barcode": barcode})
