acc19e211f942bacb854a7381a153692e9686637
SableTheRed <isobel@audioengineer.co.uk>
2026-02-10 20:38:45 +0000

    fix signup page

--- DjangoFramework/Users/views.py
+++ DjangoFramework/Users/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, FormView
 
 
 # Create your views here.
@@ -10,10 +11,25 @@ class AccountsView(CreateView):
     success_url = reverse_lazy("home")
     template_name = "Users/accounts.html"
 
-class SignupView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("signup")
+
+class SignupView(FormView):
     template_name = "Users/signup.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("home")
+
+    def form_valid(self, form):
+        form.save()
+        authenticated_user = authenticate(
+            self.request,
+            username=form.cleaned_data.get("username"),
+            password=form.cleaned_data.get("password1")
+        )
+        if authenticated_user is not None:
+            login(self.request, authenticated_user)
+            print(f"User logged in: {self.request.user}")
+        else:
+            print("Authentication failed after signup")
+        return super().form_valid(form)
 
 class LoginView(CreateView):
     form_class = AuthenticationForm
