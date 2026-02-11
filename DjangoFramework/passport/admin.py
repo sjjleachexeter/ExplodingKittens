a7effe632fa7df72eb7a0fa8894d4fe6302cc024
olim88 <bobq4582@gmail.com>
2026-02-11 14:46:31 +0000

    add basic output of most passport data

--- DjangoFramework/passport/admin.py
+++ DjangoFramework/passport/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from passport.models import Product, Ingredient
+from passport.models import Product, Ingredient, ProductIngredient, Node, Stage, Evidence, Claim, ClaimEvidence
 
 
 # Register your models here.
@@ -9,3 +9,33 @@ class ProductAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductIngredient)
+class ProductIngredientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Node)
+class NodeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Stage)
+class StageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Evidence)
+class EvidenceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Claim)
+class ClaimAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ClaimEvidence)
+class ClaimEvidenceAdmin(admin.ModelAdmin):
+    pass
--- DjangoFramework/passport/views.py
+++ DjangoFramework/passport/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from passport.models import Product
+from passport.models import Product, ProductIngredient, Ingredient, Stage
 
 
 # Create your views here.
@@ -14,9 +14,14 @@ def display_passport(request, product_id=-1):
     # try to load product
     try:
         product = Product.objects.get(product_id=product_id)
-        context = {'product_id': product_id, 'passport_data': product}
+        ingredients = ProductIngredient.objects.filter(product_id=product.id).select_related('ingredient').order_by('-proportion')
+        stages = Stage.objects.filter(product=product.id).order_by('sequence')
+
+
+        context = {'product_id': product_id, 'passport_data': product, "ingredients": ingredients, "stages": stages }
         return render(request, "passport/passport.html", context)
     except Product.DoesNotExist:
+        # show error page if product does not exist
         context = {'product_id': product_id}
         return render(request, "passport/passportNotExisting.html", context)
 
