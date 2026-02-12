27b0f13b34114e29aa5438ed24c4b790b1d97b4c
olim88 <bobq4582@gmail.com>
2026-02-12 10:42:30 +0000

    run with ngix for https

--- DjangoFramework/DjangoFramework/settings.py
+++ DjangoFramework/DjangoFramework/settings.py
@@ -27,6 +27,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
+CSRF_TRUSTED_ORIGINS= os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS","127.0.0.1").split(",")
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 
 # Application definition
