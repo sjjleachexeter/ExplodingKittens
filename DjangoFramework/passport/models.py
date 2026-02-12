77d64c7871cd208dc4edec79fb03d6b76a713a99
Vihandu <vihanduudawatta1@gmail.com>
2026-02-12 23:31:00 +0000

    Add the ProductScan, still not connected to user

--- DjangoFramework/passport/models.py
+++ DjangoFramework/passport/models.py
@@ -129,7 +129,12 @@ class ClaimEvidence(models.Model):
     claim = models.ForeignKey(Claim, on_delete= models.CASCADE, related_name = "claim_evidence_links")
     evidence = models.ForeignKey(Evidence, on_delete =models.CASCADE, related_name = "claim_evidence_links")
 
-
+class ProductScan(models.Model):
+    id =models.UUIDField(primary_key = True, default=uuid.uuid4, editable=False)
+    product= models.ForeignKey(Product, on_delete = models.CASCADE, related_name="scans")
+    #has to be connected to user
+    user = models.ForeignKey(on_delete=models.CASCADE, related_name="scans")
+    source = models.CharField(max_length = 20, default="qr")
 
 
 
