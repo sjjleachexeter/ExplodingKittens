b99ae7a996ead8a14c42f319f8f57c72767038b9
olim88 <bobq4582@gmail.com>
2026-02-14 20:59:56 +0000

    Clean up docker code for nginx

--- DjangoFramework/DjangoFramework/settings.py
+++ DjangoFramework/DjangoFramework/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", default=0))
+DEBUG =os.environ.get("DEBUG","True") == "True"
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 CSRF_TRUSTED_ORIGINS= os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS","http://127.0.0.1").split(",")
