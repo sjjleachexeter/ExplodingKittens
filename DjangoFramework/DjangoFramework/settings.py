4e1921da205ab2dd50d23ef4f9767057f84a440a
olim88 <bobq4582@gmail.com>
2026-02-16 21:13:09 +0000

    fix dev secrete key

--- DjangoFramework/DjangoFramework/settings.py
+++ DjangoFramework/DjangoFramework/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY","django-insecure-a+fw(g$qeij84ub3&5d!t=#(3g%)_wt$qtd^g+l%3k=27(%rmv")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =os.environ.get("DEBUG","True") == "True"
