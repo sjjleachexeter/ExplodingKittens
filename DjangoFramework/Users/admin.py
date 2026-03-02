6ad1fc8866b2a415973b1b79ce2967f8b8b185f5
Zachary-F201 <zjmf201@exeter.ac.uk>
2026-03-02 16:23:07 +0000

    Started creating RBAC for the project

--- DjangoFramework/Users/admin.py
+++ DjangoFramework/Users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import Role,Permission
 
 # Register your models here.
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code_name')
