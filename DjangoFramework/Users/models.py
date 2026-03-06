0bb65de30817961bf6d14bd9fc55e1f8ca294e38
olim88 <bobq4582@gmail.com>
2026-03-06 10:18:57 +0000

    Revert "Started creating RBAC for the project"
    This reverts commit 6ad1fc8866b2a415973b1b79ce2967f8b8b185f5.
    
    remove breaking changes

--- DjangoFramework/Users/models.py
+++ DjangoFramework/Users/models.py
@@ -1,18 +1,3 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
-class Role(models.Model):
-    name = models.CharField(max_length = 50, unique = True)
-    description = models.TextField()
-
-class CustomUser(AbstractUser):
-    roles = models.ManyToManyField(Role)
-
-class Permission(models.Model):
-    name = models.CharField(max_length = 100, unique = True)
-    code_name  = models.CharField(max_length=100, unique = True)
-
-class RolePermission(models.Model):
-    role = models.ForeignKey(Role, on_delete=models.CASCADE)
-    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
