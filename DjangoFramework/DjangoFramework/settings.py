98d784de9b0274d33fec84769f72ae95065ff889
olim88 <bobq4582@gmail.com>
2026-03-19 20:27:49 +0000

    fix docker

--- DjangoFramework/DjangoFramework/settings.py
+++ DjangoFramework/DjangoFramework/settings.py
@@ -131,6 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = "/"
