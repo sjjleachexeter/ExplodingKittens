a199c9dcdba6dc695030b8d7f43f9bd1040054f1
olim88 <bobq4582@gmail.com>
2026-03-23 10:36:23 +0000

    fix points per scan

--- DjangoFramework/Scanner/views.py
+++ DjangoFramework/Scanner/views.py
@@ -20,7 +20,7 @@ def load_passport(request):
             print("user is logged in")
             try:
                 product = Product.objects.get(qr_token=barcode)
-                ProductScan.objects.update_or_create(
+                _, created = ProductScan.objects.update_or_create(
                     user=request.user,
                     product=product,
                     defaults={
@@ -30,15 +30,14 @@ def load_passport(request):
                 )
                 
                 #only award points on a new scan
+
                 if created:
                     level = request.user.current_level
                     points_earned = 10
-
                     #extra points if at least one claim with evidence
                     has_evidenced_claim = product.claims.filter(missing_evidence=False).exists()
                     if has_evidenced_claim:
                         points_earned += 10
-
                     level.points += points_earned
                     level.update_level()
         
